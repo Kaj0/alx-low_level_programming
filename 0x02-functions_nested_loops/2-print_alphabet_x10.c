6fc809081375f661de90b488a2183313568f124a
Kaj0 <69155988+Kaj0@users.noreply.github.com>
2023-03-23 13:38:14 +0100

    print_alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - prints the alphabet in lower case ten times
+  */
+void print_alphabet_x10(void)
+{
+	for(int i = 0, i < 10, i++)
+	{
+		print_alphabet();
+	}
+}
