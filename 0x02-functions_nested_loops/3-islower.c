2e6032a40ddb40caa1bb62e91793baeea4ab478f
kaj0 <kareemojoaishah@gmail.com>
2023-03-23 06:59:26 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,17 @@
 
 /**
   * _islower - checks for lowercase character
+  * @c: single letter input
   * Return: 0
   */
-int _islower(void)
+int _islower(int c)
 {
-	int c;
-
 	if (c <= 'z' && c >= 'a')
 	{
 		return (1);
-	}
+
 	else
-	{
+
 		return (0);
 	}
 }
