f786ac58609284cb2fc792a945ea985db8610ca8
Kaj0 <69155988+Kaj0@users.noreply.github.com>
2023-04-18 04:33:59 +0100

    new files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - describes dog
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  *
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+  * dog_t - new name for the type struct dog
+  *
+  */ 
+typedef struct dog dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
