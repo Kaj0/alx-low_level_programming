25a814fe7e852e606fe5a1ba7ca576fac378e58b
Kaj0 <69155988+Kaj0@users.noreply.github.com>
2023-05-17 07:26:04 +0100

    unfinished

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * get_endianness - checks endianness
+  *
+  * Return: 0 for big, 1 for little endian
+  */
+int get_endianness(void)
+{
+  unsigned int x = 1;
+  char *c = (char*) &x;
+  return (int)*c;
+}
