79df9a36f32bb63ac2df3caef57ee8b24b94c792
kaj0 <kareemojoaishah@gmail.com>
2023-05-17 03:43:56 -0700

    new file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -49,15 +49,11 @@ int main(int argc, char *argv[])
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-		{
 			error_file(-1, 0, argv);
-		}
 		nwr = write(file_to, buf, nchars);
 
 		if (nwr == -1)
-		{
 			error_file(0, -1, argv);
-		}
 	}
 
 	err_close = close(file_from);
@@ -66,11 +62,5 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
 	return (0);
